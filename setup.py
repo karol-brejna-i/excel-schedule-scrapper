bb7f0b042223293ca15dba98f2d1ee3c1b21cb9a
karol-brejna-i <karolbrejna@apache.org>
2020-12-28 09:56:22 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='scrapper',
+    version='0.1.0',
+    packages=['scrapper', 'scrapper.model', 'scrapper.utils', 'scrapper.walker'],
+    url='https://github.com/karol-brejna-i/excel-schedule-scrapper',
+    license='Apache 2.0',
+    author='gruby',
+    author_email='',
+    description='For personal use. Extracts some data from excel files.'
+)
